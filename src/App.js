48d50c8d337cd674d881663bd2ae724a1c8a7901
Hilton Shumway <hillshum@gmail.com>
2017-07-22 09:27:20 -0600

    Pick number of gamber's dice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
 
+import RiggedDie from 'gamblers-dice'
+import shuffle from 'shuffle-array'
+
 import Die from './components/die'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.dice = Array(8).fill().map(()=>new RiggedDie(3))
+    this.state = {
+      rolls: [],
+      numDice: 6,
+    }
+    this.reroll = this.reroll.bind(this)
+  }
+
+
+  reroll() {
+    const toShuffle = [...this.dice]
+    shuffle(toShuffle)
+    toShuffle.splice(this.state.numDice)
+    this.setState({
+      rolls: toShuffle.map(die=>die.roll() -1)
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Gamber's Dice for Betrayal at House on the Hill</h2>
         </div>
         <div className="main">
-          {Array(6).fill({rolled: 2}).map(({rolled})=>
-            <Die rolled={rolled} />
-          )}
+          {this.state.rolls.map((roll)=><Die rolled={roll} />)}
         </div>
+        <select value={this.state.numDice} onChange={({target:{value}})=>{
+          this.setState({numDice: value})}}>
+          {Array(8).fill().map((num, index)=>(
+            <option key={index}>{index + 1}</option>
+          ))}
+        </select>
+        <button onClick={this.reroll}>Re roll</button>
       </div>
     );
   }
